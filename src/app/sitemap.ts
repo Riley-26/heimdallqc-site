56f508a8f70396400dc68b70a7429486121429bc
Riley Read <rileyread1@gmail.com>
2025-09-08 17:24:59 +0100

    build

---
+++ src/app/sitemap.ts
@@ -0,0 +1,67 @@
+// app/sitemap.ts
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: 'https://heimdallqc.com',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 1,
+        },
+        {
+            url: 'https://heimdallqc.com/about',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.8,
+        },
+        {
+            url: 'https://heimdallqc.com/features',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.8,
+        },
+        {
+            url: 'https://heimdallqc.com/pricing',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.9,
+        },
+        {
+            url: 'https://heimdallqc.com/contact',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5,
+        },
+        {
+            url: 'https://heimdallqc.com/help',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5,
+        },
+        {
+            url: 'https://heimdallqc.com/privacy',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.7,
+        },
+        {
+            url: 'https://heimdallqc.com/verif-checker',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.4,
+        },
+        {
+            url: 'https://heimdallqc.com/api-docs',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5,
+        },
+        {
+            url: 'https://heimdallqc.com/account',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5,
+        },
+    ]
+}
